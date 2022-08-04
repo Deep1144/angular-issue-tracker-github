2032987077a6c71392027d93059ade39f39a3ead
Deep <pateldeep5724@gmail.com>
2022-08-04 14:09:06 +0530

    filter and sorting

--- src/pages/IssueList.tsx
+++ src/pages/IssueList.tsx
@@ -1,4 +1,4 @@
-import { Spin, Table } from 'antd';
+import { Spin, Table, TableProps } from 'antd';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { toast } from 'react-toastify';
@@ -17,6 +17,9 @@ const columns: ColumnsType<IIssues> = [
         title: 'Comment Count',
         dataIndex: 'comments',
         key: 'comments',
+        sortDirections: ['ascend', 'descend'],
+        sorter: true,
+        defaultSortOrder: 'descend'
     },
     {
         title: 'Issue Number',
@@ -38,14 +41,24 @@ const columns: ColumnsType<IIssues> = [
                     {text.toUpperCase()}
                 </Tag>
             );
-        }
+        },
+        filters: [
+            {
+                text: <span>Closed</span>,
+                value: 'closed',
+            },
+            {
+                text: <span>Open</span>,
+                value: 'open',
+            },
+        ],
+        filterSearch: true,
     },
     {
         title: 'Date',
         dataIndex: 'date',
         width: '15%',
         render: (text, record) => {
-            console.log("text", text)
             let data = record.state === 'closed' ? 'Closed at: ' + new Date(record.closed_at).toDateString() : 'Created at: ' + new Date(record.created_at).toDateString();
             return data;
         }
@@ -90,14 +103,21 @@ const IssueList = ({ repoUrl }: IIssueList) => {
     }
 
 
-    const getIssueList = async (page = 1, pageSize = 30) => {
+    const getIssueList = async (page = 1, pageSize = 30, sortField = 'comments', sortOrder: 'ascend' | 'descend' = 'descend', state: string = '') => {
         try {
-            //' + '+type:issue'
+            const order = sortOrder === 'descend' ? 'desc' : 'asc';
             setPaginationLoader(true);
-            const response = await axios.get('https://api.github.com/search/issues?q=repo:' + repoUrl + '+type:issue', {
+
+            let apiUrl = 'https://api.github.com/search/issues?q=repo:' + repoUrl + '+type:issue';
+            if (state) {
+                apiUrl = apiUrl + '+state:' + state;
+            }
+            const response = await axios.get(apiUrl, {
                 params: {
                     per_page: pageSize,
-                    page
+                    page,
+                    sort: sortField,
+                    order
                 }
             });
             setTotalCount(response.data.total_count)
@@ -110,9 +130,19 @@ const IssueList = ({ repoUrl }: IIssueList) => {
         }
     }
 
-    const onPageChange = async (page: number, pageSize: number) => {
-        console.log({ page, pageSize });
-        await getIssueList(page, pageSize);
+
+    const handleTableStateChange = (pagination: any, filters: any, sorter: any) => {
+        let state = '';
+
+        // if 1 item is selected say open of closed then we include the filter
+        // otherwise by default it will return all issues
+        if (filters['state'] && filters['state'].length === 1) {
+            state = filters['state'][0];
+        }
+
+        if (sorter?.column && sorter?.order) {
+            getIssueList(pagination.current, pagination.pageSize, sorter.key, sorter?.order, state);
+        }
     }
 
     return (
@@ -134,12 +164,13 @@ const IssueList = ({ repoUrl }: IIssueList) => {
 
 
                     <div className='mt-20'>
-                        <Table dataSource={issueList} columns={columns} loading={paginationLoader} pagination={{
-                            total: totalCount,
-                            pageSize: 30,
-                            pageSizeOptions: [],
-                            onChange: onPageChange,
-                        }} />
+                        <Table dataSource={issueList} columns={columns} loading={paginationLoader}
+                            onChange={handleTableStateChange}
+                            pagination={{
+                                total: totalCount,
+                                pageSize: 30,
+                                pageSizeOptions: [],
+                            }} />
                     </div>
                 </div>
             }
